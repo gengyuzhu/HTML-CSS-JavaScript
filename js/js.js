7030970bc4e6c267da98710a7cda2ce1f772cf9b
gengyuzhu <126759949+gengyuzhu@users.noreply.github.com>
2023-05-10 21:34:16 +1000

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,86 @@
+function pages() {
+  document.getElementById("popOut").style.display = "block";
+}
+function pagese() {
+  document.getElementById("popOut").style.display = "none";
+}
+function tab1() {
+  document.getElementById("tab1").style.display = "block";
+  document.getElementById("tab2").style.display = "none";
+  document.getElementById("tab3").style.display = "none";
+  document.getElementById("tab4").style.display = "none";
+  document.getElementById("tab5").style.display = "none";
+  document.getElementById("tab6").style.display = "none";
+  document.getElementById("tab7").style.display = "none";
+  document.getElementById("tab8").style.display = "none";
+}
+function tab2() {
+  document.getElementById("tab1").style.display = "none";
+  document.getElementById("tab2").style.display = "block";
+  document.getElementById("tab3").style.display = "none";
+  document.getElementById("tab4").style.display = "none";
+  document.getElementById("tab5").style.display = "none";
+  document.getElementById("tab6").style.display = "none";
+  document.getElementById("tab7").style.display = "none";
+  document.getElementById("tab8").style.display = "none";
+}
+function tab3() {
+  document.getElementById("tab1").style.display = "none";
+  document.getElementById("tab2").style.display = "none";
+  document.getElementById("tab3").style.display = "block";
+  document.getElementById("tab4").style.display = "none";
+  document.getElementById("tab5").style.display = "none";
+  document.getElementById("tab6").style.display = "none";
+  document.getElementById("tab7").style.display = "none";
+  document.getElementById("tab8").style.display = "none";
+}
+function tab4() {
+  document.getElementById("tab1").style.display = "none";
+  document.getElementById("tab2").style.display = "none";
+  document.getElementById("tab3").style.display = "none";
+  document.getElementById("tab4").style.display = "block";
+  document.getElementById("tab5").style.display = "none";
+  document.getElementById("tab6").style.display = "none";
+  document.getElementById("tab7").style.display = "none";
+  document.getElementById("tab8").style.display = "none";
+}
+function tab5() {
+  document.getElementById("tab1").style.display = "none";
+  document.getElementById("tab2").style.display = "none";
+  document.getElementById("tab3").style.display = "none";
+  document.getElementById("tab4").style.display = "none";
+  document.getElementById("tab5").style.display = "block";
+  document.getElementById("tab6").style.display = "none";
+  document.getElementById("tab7").style.display = "none";
+  document.getElementById("tab8").style.display = "none";
+}
+function tab6() {
+  document.getElementById("tab1").style.display = "none";
+  document.getElementById("tab2").style.display = "none";
+  document.getElementById("tab3").style.display = "none";
+  document.getElementById("tab4").style.display = "none";
+  document.getElementById("tab5").style.display = "none";
+  document.getElementById("tab6").style.display = "block";
+  document.getElementById("tab7").style.display = "none";
+  document.getElementById("tab8").style.display = "none";
+}
+function tab7() {
+  document.getElementById("tab1").style.display = "none";
+  document.getElementById("tab2").style.display = "none";
+  document.getElementById("tab3").style.display = "none";
+  document.getElementById("tab4").style.display = "none";
+  document.getElementById("tab5").style.display = "none";
+  document.getElementById("tab6").style.display = "none";
+  document.getElementById("tab7").style.display = "block";
+  document.getElementById("tab8").style.display = "none";
+}
+function tab8() {
+  document.getElementById("tab1").style.display = "none";
+  document.getElementById("tab2").style.display = "none";
+  document.getElementById("tab3").style.display = "none";
+  document.getElementById("tab4").style.display = "none";
+  document.getElementById("tab5").style.display = "none";
+  document.getElementById("tab6").style.display = "none";
+  document.getElementById("tab7").style.display = "none";
+  document.getElementById("tab8").style.display = "block";
+}
